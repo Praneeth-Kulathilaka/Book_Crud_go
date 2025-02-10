bf6ff4075b814e67c63f00a07896643f2a6a430c
Praneeth-Kulathilaka <praneethskulathilaka@gmail.com>
2025-02-10 10:53:23 +0530

    Redis store connected

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"BookApi/config"
 	"BookApi/handlers"
+	"fmt"
+	"log"
+	"net/http"
+
 	// "BookApi/handlers/external"
 
 	"github.com/gorilla/mux"
@@ -11,6 +14,21 @@ import (
 
 func main() {
 	r := mux.NewRouter()
+
+	client := config.InitRedis()
+
+	if client == nil {
+		log.Println("Error getting redis client ")
+		return
+	}
+
+	// err := client.Set(config.Ctx, "testKey1", "testValue1", 0).Err()
+	// if err != nil {
+	// 	log.Println("Error seting key value", err)
+	// 	return
+	// }
+
+
 
 	r.HandleFunc("/books", handlers.GetAllBooks).Methods("GET")
 	r.HandleFunc("/books", handlers.CreateBook).Methods("POST")
